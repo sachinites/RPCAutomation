87248921010079662d45efce3d779c21612198c5
Abhishek Sagar <sachinites@gmail.com>
2017-03-26 00:07:36 +0530

    Comments in read me

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 rm *.o
 rm *xdr*
 rm *_t.h
-rm rpc_client_stubs.c rpc_server_init.c rpc_server_stubs.c rpc_spec.h
+rm rpc_client_stubs.c rpc_server_init.c rpc_server_stubs.c rpc_spec.h rpc_server_stubs.h
 rm client server
